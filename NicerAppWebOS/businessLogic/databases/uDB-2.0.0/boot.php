dc7c345011439d01fe9329412f5c3b5539d58120
Rene AJM Veerman <rene.veerman.netherlands@gmail.com>
2026-01-28 02:15:54 +0100

    more preliminary work done on uDB2

---
+++ NicerAppWebOS/businessLogic/databases/uDB-2.0.0/boot.php
@@ -0,0 +1,13 @@
+<?php
+require_once (dirname(__FILE__).'/class.uDB-2.0.0.php');
+
+class class_NicerAppWebOS_uDB_classHolder {
+    public $cn = 'class_NicerAppWebOS_uDB_classHolder';
+    public $c = [];
+
+    public __construct($p) {
+        $c['PHP:class_NicerAppWebOS_uDB_2_0_0'] = new class_NicerAppWebOS_uDB_2_0_0 ($p);
+    }
+}
+
+?>
---
+++ NicerAppWebOS/businessLogic/databases/uDB-2.0.0/class.uDB-2.0.0.php
@@ -0,0 +1,13 @@
+<?php
+class class_NicerAppWebOS_uDB_2_0_0 {
+    public $cn = "class_NicerAppWebOS_uDB_2_0_0";
+
+    public __construct ($p) {
+
+    }
+
+    public function loadSave_dataFolder ($p) {
+
+    }
+}
+?>
